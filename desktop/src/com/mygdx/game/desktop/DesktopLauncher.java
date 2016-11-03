e62043e55608a08b1a7dc327a47f7543c7404837
Aphisit <Aphisit@LAPTOP-IUL0E3IV>
2016-11-03 23:25:43 +0700

    Background

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.GameScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+        config.width = 1600;
+        config.height = 900;
+		new LwjglApplication(new GameScreen(), config);
 	}
 }
