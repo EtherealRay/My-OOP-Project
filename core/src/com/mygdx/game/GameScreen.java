01a2603222f35099bd8ccd146fcdd6d84da058cb
Aphisit <Aphisit@LAPTOP-IUL0E3IV>
2016-11-11 04:15:44 +0700

    Add Sound Effect

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -3,6 +3,8 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -23,7 +25,10 @@ public class GameScreen extends ApplicationAdapter {
 	int winner;
 	boolean trigger=true;
 	long time = System.currentTimeMillis();
-	long randomNum = time+MathUtils.random(5000, 10000);
+	long randomNum = time+MathUtils.random(8000, 10000);
+	Music themeSound;
+	Music duelSound;
+	Sound gunSound;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -33,17 +38,22 @@ public class GameScreen extends ApplicationAdapter {
 		player1 = new Texture("player1win.png");
 		player2 = new Texture("player2win.png");
 		duel = new Texture("duel.png");
-		//gg
+		themeSound = Gdx.audio.newMusic(Gdx.files.internal("ThemeSong.mp3"));
+		duelSound = Gdx.audio.newMusic(Gdx.files.internal("DuelSound.ogg"));
+		gunSound = Gdx.audio.newSound(Gdx.files.internal("GunShot.wav"));
 	}
 
 	@Override
 	public void render () {
+		themeSound.play();
+		themeSound.setVolume(0.2f);
 		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 
         if(Gdx.input.isKeyPressed(Keys.D)&&trigger){
+        	gunSound.play(0.5f);
         	if(System.currentTimeMillis()<=randomNum){
         		winner=2;
         	} else {
@@ -52,6 +62,7 @@ public class GameScreen extends ApplicationAdapter {
         	trigger=false;
         }
         if(Gdx.input.isKeyPressed(Keys.L)&&trigger){
+        	gunSound.play(0.5f);
         	if(System.currentTimeMillis()<=randomNum){
         		winner=1;
         	} else {
@@ -66,10 +77,16 @@ public class GameScreen extends ApplicationAdapter {
 		batch.draw(mccree2, WIDTH-600, 0);
 		if(System.currentTimeMillis()>=randomNum){
 			batch.draw(duel,810,250);
+			duelSound.play();
+			if(System.currentTimeMillis()>=randomNum+2000){
+				duelSound.stop();
+			}
 		}
 		duel(winner);
 		
 		batch.end();
+		
+
 		
 
 	}
