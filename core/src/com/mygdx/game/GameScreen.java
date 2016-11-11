f808467a39ca8e059c05771909b59926d5755e30
Aphisit <Aphisit@10.2.11.131>
2016-11-11 16:11:08 +0700

    Refactorings

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -38,20 +38,49 @@ public class GameScreen extends ApplicationAdapter {
 		player1 = new Texture("player1win.png");
 		player2 = new Texture("player2win.png");
 		duel = new Texture("duel.png");
-//		themeSound = Gdx.audio.newMusic(Gdx.files.internal("ThemeSong.mp3"));
+		themeSound = Gdx.audio.newMusic(Gdx.files.internal("ThemeSong.mp3"));
 		duelSound = Gdx.audio.newMusic(Gdx.files.internal("DuelSound.ogg"));
 		gunSound = Gdx.audio.newSound(Gdx.files.internal("GunShot.wav"));
 	}
 
 	@Override
 	public void render () {
-//		themeSound.play();
-//		themeSound.setVolume(1.0f);
-		
+		themeSound.play();
+		themeSound.setVolume(1.0f);
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gunFire();
+        batch.begin();
+		batch.draw(bg, 0, 0);
+		batch.draw(mccree, 0, 0);
+		batch.draw(mccree2, WIDTH-600, 0);
+		duelBegin();
+		duelWin(winner);	
+		batch.end();
+		
 
-
+	}
+	
+	private void duelBegin(){
+		if(System.currentTimeMillis()>=randomNum){
+			batch.draw(duel,810,250);
+			duelSound.play();
+			if(System.currentTimeMillis()>=randomNum+2000){
+				duelSound.stop();
+			}
+		}
+	}
+	
+    private void duelWin(int winner) {
+        if(winner==1) {
+    		batch.draw(player1, 538, 600);
+        }
+        if(winner==2) {
+    		batch.draw(player2, 538, 600);
+        }
+    }
+    
+    private void gunFire(){
         if(Gdx.input.isKeyPressed(Keys.D)&&trigger){
         	gunSound.play(0.5f);
         	if(System.currentTimeMillis()<=randomNum){
@@ -61,6 +90,7 @@ public class GameScreen extends ApplicationAdapter {
         	}
         	trigger=false;
         }
+        
         if(Gdx.input.isKeyPressed(Keys.L)&&trigger){
         	gunSound.play(0.5f);
         	if(System.currentTimeMillis()<=randomNum){
@@ -70,32 +100,7 @@ public class GameScreen extends ApplicationAdapter {
         	}
         	trigger=false;
         }
-
-        batch.begin();
-		batch.draw(bg, 0, 0);
-		batch.draw(mccree, 0, 0);
-		batch.draw(mccree2, WIDTH-600, 0);
-		if(System.currentTimeMillis()>=randomNum){
-			batch.draw(duel,810,250);
-			duelSound.play();
-			if(System.currentTimeMillis()>=randomNum+2000){
-				duelSound.stop();
-			}
-		}
-		duel(winner);
-		
-		batch.end();
-		
-
-	}
-	
-    private void duel(int winner) {
-        if(winner==1) {
-    		batch.draw(player1, 538, 600);
-        }
-        if(winner==2) {
-    		batch.draw(player2, 538, 600);
-        }
+    	
     }
 	
 }
